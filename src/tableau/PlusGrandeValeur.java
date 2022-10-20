8a0b338d9ea9975f7ab3803e3425c0e93f33fd49
FranckNk <nkeuboufranck@gmail.com>
2022-10-20 09:38:44 -0300

    Finalisation du projet et ajout de la fonction pour faire 'pause' sur l'écran.

--- src/tableau/PlusGrandeValeur.java
+++ src/tableau/PlusGrandeValeur.java
@@ -13,28 +13,51 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class PlusGrandeValeur {
+    // Déclarons les variables.
     static int Max           = 100;
     static int Min           =  0;
     static int TailleTableau = 15;
 
+    // Affichons le tableau.
     private static void AfficheTableau(Integer [] Tableau){
         for (int i = 0; i < Tableau.length; i++) {
             System.out.format("Tableau[%2d] = %2d\n", i, Tableau[i]);
         }
     }
-
+    // Remplissons le tableau de manière aléatoire.
     private static void RemplirRandTableau(Integer [] Tableau){
         Random rand = new Random();
-        int NombreAleatoire = 0;
         for (int i = 0; i < Tableau.length; i++) {
             Tableau[i] = rand.nextInt(Max - Min + 1) + Min;
         } 
     }
+    // Cherchons l'index du plus grand élément du tableau.
+    private static int FindMaxIndex(Integer [] Tableau){
+        // Supposons que le premier élément est le plus grand.
+        int MaxValue = 0;
+        for (int i = 0; i < Tableau.length; i++) {
+            if(Tableau[MaxValue] < Tableau[i])
+                MaxValue = i;
+        }
+        return MaxValue;
+    }
+    // fonction pour faire une pause sur l'écran.
+    static void Pause(){
+        System.out.print("\nPress any key to continue...");
+        Scanner pause = new Scanner(System.in);
+        pause.nextLine();
+    }
 
     public static void main(String[] args) throws Exception {
-        Integer Table [] = new Integer[TailleTableau];
+        int IndexMax     = 0;
+        Integer Table []     = new Integer[TailleTableau];
         RemplirRandTableau(Table);
         AfficheTableau(Table);
-        
+        // Récupérons les informations du max du tableau.
+        IndexMax = FindMaxIndex(Table);
+        // Maintenant affichons les dernières informations.
+        System.out.println("Le plus grand nombre du tableau est : " + Table[IndexMax]);
+        System.out.println("Il est à l'index : " + IndexMax);
+        Pause();
     }
 }
